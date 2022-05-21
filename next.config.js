028e34e5fb73929d398c07ea953d9e5c5bebf6e1
Shaun Yap <shaun.yap@amperity.com>
2022-05-21 09:54:00 -0700

    get global variabels to work
    Signed-off-by: Shaun Yap <shaun.yap@amperity.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+}
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@use 'global' as *; @use 'global/variables/colors';`
+  },
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from './Home.module.scss'
 
 export default function Home() {
   return (
@@ -12,9 +12,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.title} >
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <p className="large">hi there</p>
 
         <p className={styles.description}>
           Get started by editing{' '}
